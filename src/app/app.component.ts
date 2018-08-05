fa7e1a5a7180f5bed8400e6aec7a8fa70664f929
Haritha Wickremasinghe <haritha.wickremasinghe@ifsworld.com>
2018-08-05 22:30:55 +0530

    Adding support for creating dictionary entries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataserviceService } from './dataservice.service';
+import { concat } from 'rxjs';
+import { switchMap, tap } from '../../node_modules/rxjs/operators';
+import { DictionaryEntry } from './app.interfaces';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'my-dictionary';
+  model: DictionaryEntry = { word: '', definition: ''};
+  ongoingSubmit = false;
+  constructor(private dataService: DataserviceService) {
+    //
+  }
+
+  public ngOnInit(): void {
+    //
+  }
+
+  public onSubmit() {
+    this.ongoingSubmit = true;
+    this.dataService.addEntry({ word: this.model.word, definition: this.model.definition})
+    .subscribe(
+      e => { alert('Added successfully'); this.ongoingSubmit = false; }
+    , e => { alert ('Could not add word to dictionary');  this.ongoingSubmit = false; });
+  }
+
 }
---
+++ src/app/app.interfaces.ts
@@ -0,0 +1,5 @@
+export interface DictionaryEntry {
+    word: string;
+    definition: string;
+    tags?: string[];
+}
