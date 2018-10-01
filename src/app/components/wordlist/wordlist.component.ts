ddf2e7182b5d6f0750d1ebaa7d7ebef1c66c59bc
gawicks <gawicks@gmail.com>
2018-10-01 17:45:54 +0530

    Word list Component

--- src/app/components/wordlist/wordlist.component.ts
+++ src/app/components/wordlist/wordlist.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { DataserviceService } from '../../dataservice.service';
+import { DictionaryEntry } from '../../app.interfaces';
+import { Observable } from 'rxjs';
+import { toArray } from 'rxjs/operators';
 
 @Component({
   selector: 'app-wordlist',
   templateUrl: './wordlist.component.html',
-  styleUrls: ['./wordlist.component.css']
+  styleUrls: ['./wordlist.component.scss']
 })
 export class WordlistComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataService: DataserviceService) { }
+  public words: Observable<DictionaryEntry[]>;
 
   ngOnInit() {
+    this.words =  this.dataService.getEntries().pipe(toArray());
   }
 
 }
